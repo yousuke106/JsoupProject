9938aa83fcff3055beb8df8b3748894d4a38a371
yousuke106 <96982493+yousuke106@users.noreply.github.com>
2022-01-08 23:03:03 +0900

    HGUC対応

--- src/Main.java
+++ src/Main.java
@@ -3,24 +3,41 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+/**
+ * Javaのスクレイピングプログラムのサンプル
+ * 
+ * @author user
+ *
+ */
 public class Main {
-
+    
     public static void main(String[] args) {
-
+        
         try {
-            Document doc = Jsoup.connect("https://java-skill.com/").get();
-
-            Elements elements = doc.select("h3");
-
-            for (Element element : elements){
-                System.out.println(element);
+            Document doc = Jsoup.connect(
+                    "https://www.dmm.com/mono/hobby/-/list/=/article=keyword/id=15344/list_type=reserve/n1=DgRJTglEBQ4GwJ2dwJOxkog_/sort=date/view=text/")
+                    .get();
+            
+            Elements elements = doc.select("a");
+            
+            for (Element element : elements) {
+                if ((element.attributes().hasKey("href")) && (element.attributes().size() == 1)) {
+                    
+                    String temp = element.attributes().get("href");
+                    
+                    if (!temp.startsWith("https://www.dmm.com/mono/hobby")) {
+                        continue;
+                    }
+                    
+                    String tmp2 = element.childNodes().get(0).toString();
+                    System.out.println(tmp2);
+                }
             }
-
-
+            
         } catch (Exception e) {
             e.printStackTrace();
         }
-    
+        
     }
     
 }
